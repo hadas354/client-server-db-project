772e9ed72d99dfd7c2cba8d20d42005d69b3860d
yokheved <yokheved.spr@gmail.com>
2024-03-09 20:03:39 +0200

    db create

---
+++ server/db/create.js
@@ -0,0 +1,79 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '02042024',
+    database: 'week_21_db'
+}).promise();
+
+export const create = async () => {
+    // Create the database
+    await pool.query('CREATE DATABASE IF NOT EXISTS jsonplaceholderdemo');
+
+    // Switch to the newly created database
+    await pool.query('USE jsonplaceholderdemo');
+
+    // Create the Users table
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS Users(
+        id int,
+        name varchar(255),
+        username varchar(255),
+        email varchar(255),
+        city varchar(255),
+        phone int,
+        website varchar(255),
+        companyName varchar(255),
+        PRIMARY KEY (id)
+    );
+`);
+
+    // Create the Todos table
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS Todos(
+        userID int,
+        id int,
+        title varchar(255),
+        completed BOOLEAN,
+        PRIMARY KEY (id),
+        FOREIGN KEY (userID) REFERENCES Users(id)
+    );
+`);
+
+    // Create the Posts table
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS Posts(
+        id int,
+        userID int,
+        title varchar(255),
+        body text,
+        PRIMARY KEY (id),
+        FOREIGN KEY (userID) REFERENCES Users(id)
+    );
+`);
+
+    // Create the Comments table
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS Comments(
+        postID int,
+        id int,
+        name varchar(255),
+        body text,
+        userID int,
+        PRIMARY KEY (id),
+        FOREIGN KEY (userID) REFERENCES Users(id),
+        FOREIGN KEY (postID) REFERENCES Posts(id)
+    );
+`);
+
+    // Create the Passwards table
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS Passwards(
+        userID int,
+        passward varchar(255),
+        PRIMARY KEY (userID),
+        FOREIGN KEY (userID) REFERENCES Users(id)
+    );
+`);
+};
---
+++ server/db/run.js
@@ -0,0 +1,2 @@
+import {create} from './create.js';
+create();
